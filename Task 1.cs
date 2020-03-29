d5a8a856ff3bc7999abc3e33ef54ceddc0c34a4d
hayariel <62279688+hayariel@users.noreply.github.com>
2020-03-29 22:22:45 +0300

    Add files via upload

---
+++ Task 1.cs
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.29911.84
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Task 1", "Task 1\Task 1.csproj", "{05382287-C784-422C-A2C6-89CEDE8636F3}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{05382287-C784-422C-A2C6-89CEDE8636F3}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{05382287-C784-422C-A2C6-89CEDE8636F3}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{05382287-C784-422C-A2C6-89CEDE8636F3}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{05382287-C784-422C-A2C6-89CEDE8636F3}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {7F3F3859-DA7C-4D47-B9A9-CBBD1F0E4BB4}
+	EndGlobalSection
+EndGlobal
